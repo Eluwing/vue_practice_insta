21bad43cc766fa2f0370d81b6f0a973eec134137
Eluwing <abek0403@gmail.com>
2024-08-21 17:28:58 +0900

    [Feature]モックデータ追加

---
+++ src/assets/Posts.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Kim Hyun",
+    userImage: "https://picsum.photos/100?random=3",
+    postImage: "https://picsum.photos/600?random=3",
+    likes: 36,
+    date: "5月15日",
+    liked: false,
+    content: "今日は何をしたかというと、何もしていませんでした。",
+    filter: "perpetua"
+  },
+  {
+    name: "John Doe",
+    userImage: "https://picsum.photos/100?random=4",
+    postImage: "https://picsum.photos/600?random=4",
+    likes: 20,
+    date: "4月20日",
+    liked: false,
+    content: "よくある自慢スタグラム",
+    filter: "clarendon"
+  },
+  {
+    name: "Minny",
+    userImage: "https://picsum.photos/100?random=5",
+    postImage: "https://picsum.photos/600?random=5",
+    likes: 49,
+    date: "4月4日",
+    liked: false,
+    content: "うちの犬はトイレの水も流します。",
+    filter: "lofi"
+  }
+]
