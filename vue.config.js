6ec116abb26c2eb6f50a5a381bc04caa5d63c76c
Eluwing <abek0403@gmail.com>
2025-07-23 19:46:39 +0900

    [env]環境設定およびエラー解除

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+// vue CLIを使っているため、commonJSで作成必要
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  configureWebpack: {
+    resolve: {
+      extensions: ['.ts', '.js', '.vue'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/], // <script lang="ts"> 처리
+          },
+        },
+      ],
+    },
+  },
 })
