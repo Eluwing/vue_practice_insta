57a95ba9a08d85a46766a386020625bedef7aab8
Eluwing <abek0403@gmail.com>
2025-07-29 18:46:25 +0900

    feat:フィルターボックスコンポーネントComposition API化

--- src/views/Upload/components/filter-select/FilterBox.vue
+++ src/views/Upload/components/filter-select/FilterBox.vue
@@ -1,23 +1,23 @@
 <template>
-  <div :class="`${ filter } filter-item`" :style="`background-image:url(${ uploadFileUrl }); cursor: pointer;`" @click="fire">
+  <div :class="`${ filter } filter-item`" :style="`background-image:url(${ uploadFileUrl }); cursor: pointer;`" @click="handleFliterClick">
     <!-- 親が子コンポーネントのデータを使いたいならslot props利用 -->
     <!-- props代わりに<slot>を使ってデータバインディング可能 -->
     <slot></slot>
   </div> 
 </template>
 
-<script>
-export default {
-  name: 'FilterBox',
-  methods: {
-    fire(){
-      this.emitter.emit('clickedFilter', this.filter)
-    }
-  },
-  props: {
-    uploadFileUrl: String,
-    filter: String,
-  }
+<script setup lang="ts">
+import { useSelectedFilter } from '@/composables/useSelectedFilter'
+
+const { selectFilter } = useSelectedFilter();
+
+const props = defineProps<{
+  uploadFileUrl: string;
+  filter: string;
+}>()
+
+function handleFliterClick() {
+  selectFilter(props.filter)
 }
 </script>
 
