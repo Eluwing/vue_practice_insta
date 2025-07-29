bea91208f4abad1353807d506fb9685b4064c835
Eluwing <abek0403@gmail.com>
2025-07-29 16:44:30 +0900

    chore:path設定訂正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 // vue CLIを使っているため、commonJSで作成必要
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -7,6 +9,13 @@ module.exports = defineConfig({
   configureWebpack: {
     resolve: {
       extensions: ['.ts', '.js', '.vue'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@home': path.resolve(__dirname, 'src/views/Home'),
+        '@upload': path.resolve(__dirname, 'src/views/Upload'),
+        '@search': path.resolve(__dirname, 'src/views/Search'),
+        '@activity': path.resolve(__dirname, 'src/views/Activity'),
+      },
     },
     module: {
       rules: [
@@ -15,7 +24,7 @@ module.exports = defineConfig({
           loader: 'ts-loader',
           exclude: /node_modules/,
           options: {
-            appendTsSuffixTo: [/\.vue$/], // <script lang="ts"> 처리
+            appendTsSuffixTo: [/\.vue$/], // <script lang="ts"> 処理
           },
         },
       ],
